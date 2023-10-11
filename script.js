74093271faa181991038d9ed2215097c5413dcc8
rutgerkock <kock.rutger@gmail.com>
2023-10-11 11:34:29 +0200

    Updates veel

--- script.js
+++ script.js
@@ -25,11 +25,13 @@ setTimeout(function () {
 document.addEventListener('DOMContentLoaded', function() {
     var dropdowns = document.querySelectorAll('.dropdown');
     var body = document.body;
+    var dropdownButton = document.querySelector('.dropdown-button');
 
     dropdowns.forEach(function(dropdown) {
         var dropdownContent = dropdown.querySelector('.dropdown-content');
 
         dropdown.addEventListener('click', function() {
+            dropdownButton.classList.toggle('open');
             if (dropdownContent.style.height === '0px' || dropdownContent.style.height === '') {
                 dropdownContent.style.height = dropdownContent.scrollHeight + 'px';
             } else {
@@ -46,6 +48,8 @@ meerLezenButtons.forEach(function(button) {
     button.addEventListener("click", function(event) {
         event.preventDefault();
         var extraText = button.parentElement.querySelector(".extraText");
+        button.classList.toggle("open");
+
         if (extraText.style.display === "none" || extraText.style.display === "") {
             extraText.style.display = "block";
         } else {
